2dece4e938e2337142f7eb4d774047e29bfddbf0
Spacier829 <mrsimpleeegor@gmail.com>
2024-12-17 16:09:29 +0700

    feat(main): testing redirect

--- src/main/java/maven/example/com/jakartatest/FirstServlet.java
+++ src/main/java/maven/example/com/jakartatest/FirstServlet.java
@@ -13,6 +13,14 @@ public class FirstServlet extends HttpServlet {
   }
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+//    response.sendRedirect("https://github.com/JakartaTest/jakartatest");
+//    response.sendRedirect("/testJsp.jsp");
+//    response.setContentType("text/html");
+//
+//    // Hello
+//    PrintWriter out = response.getWriter();
+//    out.println("<html><body>");
+//    out.println("<h1>" + "First Servlet" + "</h1>");
+//    out.println("</body></html>");
   }
 }
